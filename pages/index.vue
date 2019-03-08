a3f5fbf909772eb331026e7703f2ea0512f6eac7
s-peace <ssmim777@gmail.com>
2019-03-08 22:54:45 +0900

    1st commit

---
+++ pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="container">
+    <h1>{{title}}</h1>
+    <p>{{message}}</p>
+    <hr>
+    <router-link to="/other">Go to Other</router-link>
+  </section>
+</template>
+
+<script>
+export default {
+  data: function(){
+    return {
+      title: 'hello',
+      message: 'this is message.',
+      now: 'wait...',
+    };
+  },
+  created: function(){
+    setInterval(()=>{
+      let d = new Date();
+      this.now = d.getHours()
+                + ':' + d.getMinutes()
+                + ':' + d.getSeconds();
+    },1000);
+  },
+};
+</script>
+
+<style>
+.container {
+  padding: 5px 10px;
+}
+h1 {
+  font-size: 60px;
+  color: #345980;
+}
+p {
+  padding-top: 5px;
+  color: #345980;
+}
+pre {
+  padding: 10px;
+  font-size: 18px;
+  background-color: #efefef;
+}
+hr {
+  margin: 10px 0;
+}
+
+</style>
