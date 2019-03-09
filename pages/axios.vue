35e98f5a0806cae2c9fb4576eac9e992c9401f28
s-peace <ssmim777@gmail.com>
2019-03-10 01:50:20 +0900

    axios

---
+++ pages/axios.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="container">
+    <h1>{{title}}</h1>
+    <p>{{message}}</p>
+    <pre>{{html_data}}</pre>
+  </section>
+</template>
+
+<script>
+  const axios = require('axios');
+
+  let url = "/README.md";
+
+  export default {
+    data: function(){
+      return {
+        title: 'Axios',
+        message: 'axios sample',
+      };
+    },
+    asyncData: async function(){
+      let result = await axios.get(url);
+      return {html_data: result.data};
+    },
+  }
+</script>
+
+<style>
+.container {
+  padding: 5px 10px;
+}
+h1 {
+  font-size: 60px;
+  color: #345980;
+}
+p {
+  padding-top: -5px;
+  font-size: 20px;
+}
+div {
+  font-size: 14px;
+}
+pre {
+  padding: 10px;
+  font-size: 18px;
+  background-color: #efefef;
+  word-spacing: pre-wrap;
+}
+hr {
+  margin: 10px 0;
+}
+</style>
+
+
