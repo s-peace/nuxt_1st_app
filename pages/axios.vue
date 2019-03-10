b837d396c86309a2636778a3895c7f32f781ff37
s-peace <ssmim777@gmail.com>
2019-03-11 01:20:15 +0900

    axios3

--- pages/axios.vue
+++ pages/axios.vue
@@ -2,25 +2,60 @@
   <section class="container">
     <h1>{{title}}</h1>
     <p>{{message}}</p>
-    <pre>{{html_data}}</pre>
+    <div>
+      <input type="text" v-model="msg">
+      <button @click="doClick">Click</button>
+    </div>
+    <table>
+      <tr>
+        <th>User ID</th>
+        <td>{{json_data.userId}}</td>
+      </tr>
+      <tr>
+        <th>ID</th>
+        <td>{{json_data.id}}</td>
+      </tr>
+      <tr>
+        <th>Title</th>
+        <td>{{json_data.title}}</td>
+      </tr>
+      <tr>
+        <th>Body</th>
+        <td>{{json_data.body}}</td>
+      </tr>
+    </table>
   </section>
 </template>
 
 <script>
   const axios = require('axios');
 
-  let url = "/README.md";
+  let url = "https://jsonplaceholder.typicode.com/posts/";
 
   export default {
     data: function(){
       return {
         title: 'Axios',
         message: 'axios sample',
+        msg: '',
+        json_data: {},
       };
     },
-    asyncData: async function(){
-      let result = await axios.get(url);
-      return {html_data: result.data};
+    methods: {
+      doClick: function(event){
+        axios.get(url + this.msg).then((res) => {
+          this.message = 'get ID=' + this.msg;
+          this.json_data = res.data;
+        }).catch((error) => {
+          this.message = 'ERROR!';
+          this.json_data = {};
+        })
+      }
+    },
+    async asyncData(){
+      let id = 1;
+      let result = await axios.get(url + id);
+      return {json_data: result.data};
     },
   }
 </script>
@@ -48,6 +83,23 @@ pre {
 }
 hr {
   margin: 10px 0;
+}
+tr th {
+  width: 150px;
+  background-color: darkblue;
+  color: white;
+  font-size: 16px;
+}
+tr td {
+  padding: 5px 10px;
+  background-color: #eef;
+  font-size: 14px;
+}
+input {
+  font-size: 14px;
+}
+button {
+  font-size: 14px;
 }
 </style>
 
